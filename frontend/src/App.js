447347537b279becb8592cfb8d7ece355cff1237
Sperlini <Alexander.sperlin@hotmail.com>
2023-05-04 15:42:17 +0200

    api fetch app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
 import "./css/main.css"
 import Header from "./components/Header";
+import { useState , useEffect} from 'react';
 
 function App() {
+  
+  const [games, setGames] = useState([])
+
+  const getGames = async() =>{
+    
+    const response = await fetch(`https://api.rawg.io/api/games?key=28051d509bd94f1d98dfc83a47f631c4`)
+    const data = await response.json()
+    console.log(data)
+}
+
+useEffect(() =>{
+  getGames()
+},[])
+  
   return (
     <div className="App">
       <Header />
     </div>
+    
   );
+  
+  
 }
+
 
 export default App;
