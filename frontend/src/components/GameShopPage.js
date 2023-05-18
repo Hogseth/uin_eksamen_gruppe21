f7ff570f6a2fa56a42567c6aa1f78b1a0a1e947e
Sperlini <Alexander.sperlin@hotmail.com>
2023-05-18 13:52:28 +0200

    +GameShopPage

--- frontend/src/components/GameShopPage.js
+++ frontend/src/components/GameShopPage.js
@@ -1,30 +1,30 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
-export default function GameShop() {
+
+export default function GameShopPage() {
   const [games, setGames] = useState([]);
 
   useEffect(() => {
     async function fetchGames() {
       const apiKey = '9334c7d3b22742539c1b4fd26c6d27a3';
-      const response = await fetch(`https://api.rawg.io/api/games?key=${apiKey}&page=1`);
+      const response = await fetch(`https://api.rawg.io/api/games?key=${apiKey}`);
       const data = await response.json();
-      const slicedGames = data.results.slice(0, 3);
+      const gameshopGames = data.results
 
       const updatedGames = await Promise.all(
-        slicedGames.map(async (game) => {
+        gameshopGames.map(async (game) => {
         const gameResponse = await fetch(`https://api.rawg.io/api/games/${game.id}?key=${apiKey}`);
         const gameData = await gameResponse.json();
         const storeResponse = await fetch(`https://api.rawg.io/api/games/${game.id}/stores?key=${apiKey}`);
         const storeData = await storeResponse.json();
         const firstStore = storeData.results[0];
-        console.log(data)
+
 
 
 
         return {
+
             id: game.id,
-            next: data.next,
             name: gameData.name,
             genre: gameData.genres.map((genre) => genre.name).join(', '),
             image: game.background_image,
@@ -47,13 +47,8 @@ export default function GameShop() {
 
   return (
     <>
-      <div id="gameshop-title">
-        <h2>GAMESHOP</h2>
-        <Link to="gameshop">
-          <p>Visit Shop</p>
-        </Link>
-      </div>
-      <section id="shop-section">
+     
+      <section>
         {games.map((game) => (
           <div className="container" key={game.id}>
             <img className="gameImg" src={game.image} alt={game.name} />
@@ -63,10 +58,8 @@ export default function GameShop() {
               <a href='#' onClick={() => openStoreLink(game.storeUrl)} style={{ cursor: 'pointer' }}>BUY</a>
             </div>
           </div>
-          
         ))}
       </section>
-      
     </>
   );
 }
